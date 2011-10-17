227df7f7251245cb80c8b5255bacfdef47cfe21f
venturemedias <elliot@venturemedias.com>
2011-10-17 10:18:44 -0400

    add all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradeleaders::Application.config.secret_token = '064faff7a839600cf21042551a2128e0318e3fde39ee817ee1a6249bb98963d2e6eb4fcad4d044c815396ea383d5d57afb95c9e32d2f08d2c4f5715b856cf67f'
